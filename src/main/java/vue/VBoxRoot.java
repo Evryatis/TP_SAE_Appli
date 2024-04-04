bbe9e994ddf813b5d053449220dcc7ef3f72a8ca
Evryatis <48336818+Evryatis@users.noreply.github.com>
2024-04-04 11:57:37 +0200

    Update VBoxRoot ajout deplacement

--- src/main/java/vue/VBoxRoot.java
+++ src/main/java/vue/VBoxRoot.java
@@ -1,16 +1,10 @@
-package vue;
-
-import javafx.geometry.Insets;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import modele.Position;
 
 public class VBoxRoot extends VBox implements CanvasConstantes{
     Label nbre_pas = new Label();
     Canvas carte = new Canvas();
     Position positionApprenti;
+    GraphicsContext graphicsContext2D;
+
 
     public VBoxRoot(){
         Label nbre_pas = new Label("Nombre de pas : 0");
@@ -20,7 +14,7 @@ public class VBoxRoot extends VBox implements CanvasConstantes{
         Canvas canvasCarte = new Canvas();
         canvasCarte.setWidth(LARGEUR_CANVAS);
         canvasCarte.setHeight(HAUTEUR_CANVAS);
-        GraphicsContext graphicsContext2D = canvasCarte.getGraphicsContext2D();
+        graphicsContext2D = canvasCarte.getGraphicsContext2D();
 
         graphicsContext2D.setStroke(COULEUR_GRILLE);
         for (int i = 0; i < LARGEUR_CANVAS; i += CARREE){
@@ -28,11 +22,13 @@ public class VBoxRoot extends VBox implements CanvasConstantes{
                 graphicsContext2D.strokeRect(i, j, CARREE, CARREE);
             }
         }
+
+        // selection de la case et retour de la position
         canvasCarte.setOnMouseClicked(event -> {
             int abscisse = (int) event.getX() / CARREE;
             int ordonnee = (int) event.getY() / CARREE;
             Position positionCliquee = new Position(abscisse, ordonnee);
-            positionApprenti.deplacementUneCase(positionCliquee);
+            deplacementAvecTimer(positionApprenti, positionCliquee);
             System.out.println(positionCliquee.toString());
         });
 
@@ -41,6 +37,8 @@ public class VBoxRoot extends VBox implements CanvasConstantes{
             positionApprenti.getOrdonnee()*CARREE+CARREE/4,
                 LARGEUR_OVALE, HAUTEUR_OVALE
         );
+
+
 
         //les numéros de colonne
         int numCol = 1;
@@ -64,5 +62,20 @@ public class VBoxRoot extends VBox implements CanvasConstantes{
         this.getChildren().add(canvasCarte);
         VBox.setMargin(canvasCarte, new Insets(30));
     }
+    public void deplacementAvecTimer(Position parPositionApprenti, Position parPosition){
+        Timer timer = new Timer();
+        Timer timerTask = new TimerTask(){
 
-}
+            @Override
+            public void run() {
+                graphicsContext2D.clearRect(positionApprenti.getAbscisse()*CARREE + CARREE/8, positionApprenti.getOrdonnee()*CARREE+CARREE/4, LARGEUR_OVALE, HAUTEUR_OVALE);
+                positionApprenti.deplacementUneCase(parPosition);
+                graphicsContext2D.fillOval(positionApprenti.getAbscisse()*CARREE + CARREE/8, positionApprenti.getOrdonnee()*CARREE+CARREE/4, LARGEUR_OVALE, HAUTEUR_OVALE);
+                if (positionApprenti.equals(parPosition)){
+                    timer.cancel();
+                }
+            }
+        };
+        timer.scheduleAtFixedRate(timerTask, 1000, 200);
+    }
+    
